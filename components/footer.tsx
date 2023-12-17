dc876e8b4901778202cbf4fe57324eb9488541d0
Jo <jose_f_dirazar@hotmail.com>
2023-12-17 16:08:28 -0300

    repo created

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="border-t bg-white dark:border-neutral-700 dark:bg-black">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black dark:text-white">
+          &copy; 2023 Belli Deportes, Olavarría. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
