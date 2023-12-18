6bcc525cbce5362f8f065061c62b1e4a1ea0a082
Jo <jose_f_dirazar@hotmail.com>
2023-12-17 22:49:33 -0300

    footer bug

--- components/ui/no-results.tsx
+++ components/ui/no-results.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 const NoResults = () => {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center text-neutral-400">
+    <div className="flex h-full w-full flex-col min-h-[50vh] items-center justify-center text-[#A08BC9]">
       <p className="pb-3">Sin resultados.</p>
       <Link
         href="/"
-        className="rounded-full bg-black p-2 px-5 text-neutral-200 hover:scale-[1.10] hover:text-white dark:bg-neutral-200 dark:text-black dark:hover:bg-white"
+        className="rounded-full bg-[#E8D7FF] p-2 px-5 border border-[#A08BC9] hover:scale-[1.10] hover:text-white dark:bg-neutral-200 dark:text-black dark:hover:bg-white"
       >
         Home
       </Link>
